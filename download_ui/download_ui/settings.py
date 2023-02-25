5f571b7de51473e4179794c5c4bb3df73d3e87c0
kerry.nich <kerry.ann.nicholson@gmail.com>
2023-02-25 10:21:16 -0500

    Fixed key issue

--- download_ui/download_ui/settings.py
+++ download_ui/download_ui/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lh2=5#k)gu(8e$s_snkt(r!m1o6ivn7s#lk29j!fr0w^ix@6e2'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
